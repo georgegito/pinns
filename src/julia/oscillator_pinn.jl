1176b22c831d73810255eeb9170fb452dd9d2104
georgegito <gitopoulos99@gmail.com>
2023-04-27 21:06:09 +0300

    Remove todo.txt & update gitignore

--- src/julia/oscillator_pinn.jl
+++ src/julia/oscillator_pinn.jl
@@ -42,7 +42,6 @@ v0 = 0.0 # Initial velocity
 
 initial_conditions = [y(0.0) ~ y0, 
                     Dt(y(0.0)) ~ v0]
-# ics = [y(0.0) ~ y0]
 
 # Training points
 y_u = y0 .* exp_term .* osc_term
@@ -62,11 +61,7 @@ y_real = y0 .* exp_term_real .* osc_term_real
 
 p1 = plot(t_real, y_real, xlabel="t", ylabel="y", label="Real system output", linewidth=:1.5)
 p1 = scatter!(t_u, y_u, label="Training points")
-# display(p1)
 
-# Solve
-# tspan = (0.0, 20.0)
-# prob = ODEProblem(f, ics, tspan)
 chain = Lux.Chain(Lux.Dense(1, 64, Lux.σ), 
                 Lux.Dense(64, 64, Lux.σ),
                 # Lux.Dense(32, 32, Lux.σ),
@@ -78,14 +73,7 @@ if use_gpu
 end
 
 # opt = OptimizationOptimisers.Adam(0.01)
-# opt = OptimizationOptimisers.Adam(0.1, (0.9, 0.95))
 opt = Optim.BFGS()
-
-# sol = solve(prob, NeuralPDE.NNODE(chain, opt))
-# sol = solve(prob, NeuralPDE.NNODE(chain, opt), dt = 1 / 20.0f0, verbose = true,
-            # abstol = 1.0f-10, maxiters = 200)
-
-# t_f = range(0, stop=20, length=100) # collocation points
 
 # strategy = GridTraining(0.1)
 strategy = QuasiRandomTraining(200, bcs_points=length(boundary_conditions)/2)
@@ -110,13 +98,8 @@ end
 res = @time Optimization.solve(prob, opt; callback=callback, maxiters=1000)
 phi = discretization.phi
 
-# ts = [infimum(d.domain):0.01:supremum(d.domain) for d in domain]
 t_span = (0.0:0.01:20.0)
 y_predict = [first(phi([t], res.u)) for t in t_span]
 
 p1 = plot!(t_span, y_predict, xlabel="t", ylabel="y", label="PINN prediction", linewidth=:1.5, linecolor=:red, linestyle=:dot)
 plot(p1)
-# plot(p1, p2, layout=(2, 1), size=(500, 500))
-
-# p3 = p1
-# p3 = plot!(t_span, y_predict, xlabel="t", ylabel="y", label="PINN prediction")
