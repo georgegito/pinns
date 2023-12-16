1ac55b3ff46f970e23656500ad4901d5d606115d
georgegito <gitopoulos99@gmail.com>
2023-12-16 19:02:22 +0200

    Fix checkpoint loading & load automatically when loss is NaN

--- src/python/utils.py
+++ src/python/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import csv
+import os
 
 def stack_xyzt_tensors(x, y, z, t):
   return torch.stack((x, y, z, t), axis=-1)
@@ -33,3 +34,25 @@ def save_log(log_filepath, epoch, epoch_loss):
 
 def print_log(epoch, epoch_loss):
     print(f'Epoch: {epoch},\tTotal loss: {epoch_loss[0]},\tPDE loss: {epoch_loss[1]}\tIC loss: {epoch_loss[2]},\tBC loss: {epoch_loss[3]},\tNo-slip loss: {epoch_loss[4]}')
+
+def save_checkpoint(pinn, epoch, optimizer, filepath):
+  print("=> saving checkpoint '{}'".format(filepath))
+  state = {'epoch': epoch, 'state_dict': pinn.state_dict(),
+             'optimizer': optimizer.state_dict()}
+  torch.save(state, filepath)
+
+def load_checkpoint(model, optimizer, filepath):
+    # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
+    start_epoch = 0
+    if os.path.isfile(filepath):
+        print("=> loading checkpoint '{}'".format(filepath))
+        checkpoint = torch.load(filepath)
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(filepath, checkpoint['epoch']))
+    else:
+        print("=> no checkpoint found at '{}'".format(filepath))
+
+    return model, optimizer, start_epoch
