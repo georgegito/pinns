d2e218cce7be06f5dc3903b6b70db88287a00022
georgegito <gitopoulos99@gmail.com>
2024-03-20 23:48:40 +0200

    Change domain size and pdes representation

--- src/python/deepxde/navier_stokes.py
+++ src/python/deepxde/navier_stokes.py
@@ -21,62 +21,96 @@ class NavierStokesPDEs:
 
 
   def get_pdes(self, x, y):
-    """
-    System of PDEs to be minimized: incompressible Navier-Stokes equation in the
-    continuum-mechanics based formulations.
-    """
-    rho, mu = self.rho, self.mu
+      """
+      System of PDEs to be minimized: incompressible Navier-Stokes equation in the
+      continuum-mechanics based formulations.
+      """
+      rho, mu = self.rho, self.mu
 
-    # psi, p = y[:, 0:1], y[:, 1:2]
-    u, v , p = y[:, 0:1], y[:, 1:2], y[:, 2:3]
+      psi, p, sigma11, sigma22, sigma12 = y[:, 0:1], y[:, 1:2], y[:, 2:3], y[:, 3:4], y[:, 4:5]
+      
+      u =   dde.grad.jacobian(y, x, i = 0, j = 1)
+      v = - dde.grad.jacobian(y, x, i = 0, j = 0)
+      
+      u_x = dde.grad.jacobian(u, x, i = 0, j = 0)
+      u_y = dde.grad.jacobian(u, x, i = 0, j = 1)
+      
+      v_x = dde.grad.jacobian(v, x, i = 0, j = 0)
+      v_y = dde.grad.jacobian(v, x, i = 0, j = 1)
+      
+      sigma11_x = dde.grad.jacobian(y, x, i = 2, j = 0)
+      sigma12_x = dde.grad.jacobian(y, x, i = 4, j = 0)
+      sigma12_y = dde.grad.jacobian(y, x, i = 4, j = 1)
+      sigma22_y = dde.grad.jacobian(y, x, i = 3, j = 1)
+      
+      continuumx = rho * (u * u_x + v * u_y) - sigma11_x - sigma12_y
+      continuumy = rho * (u * v_x + v * v_y) - sigma12_x - sigma22_y
+      
+      constitutive1 = - p + 2 * mu * u_x - sigma11
+      constitutive2 = - p + 2 * mu * v_y - sigma22
+      constitutive3 = mu * (u_y + v_x) - sigma12
+      constitutive4 = p + (sigma11 + sigma22) / 2
+      
+      return continuumx, continuumy, constitutive1, constitutive2, constitutive3, constitutive4
 
-    # u =   dde.grad.jacobian(y, x, i = 0, j = 1)
-    # v = - dde.grad.jacobian(y, x, i = 0, j = 0)
 
-    u_x = dde.grad.jacobian(u, x, i = 0, j = 0)
-    u_y = dde.grad.jacobian(u, x, i = 0, j = 1)
+  # def get_pdes(self, x, y):
+  #   """
+  #   System of PDEs to be minimized: incompressible Navier-Stokes equation in the
+  #   continuum-mechanics based formulations.
+  #   """
+  #   rho, mu = self.rho, self.mu
 
-    v_x = dde.grad.jacobian(v, x, i = 0, j = 0)
-    v_y = dde.grad.jacobian(v, x, i = 0, j = 1)
+  #   # psi, p = y[:, 0:1], y[:, 1:2]
+  #   u, v , p = y[:, 0:1], y[:, 1:2], y[:, 2:3]
 
-    # u_xx = dde.grad.jacobian(u_x, x, i = 0, j = 0)
-    # u_yy = dde.grad.jacobian(u_y, x, i = 0, j = 1)
+  #   # u =   dde.grad.jacobian(y, x, i = 0, j = 1)
+  #   # v = - dde.grad.jacobian(y, x, i = 0, j = 0)
 
-    u_xx = dde.grad.hessian(u, x, component=0, i = 0, j = 0)
-    u_yy = dde.grad.hessian(u, x, component=0, i = 0, j = 1)
+  #   u_x = dde.grad.jacobian(u, x, i = 0, j = 0)
+  #   u_y = dde.grad.jacobian(u, x, i = 0, j = 1)
 
-    # v_xx = dde.grad.jacobian(v_x, x, i = 0, j = 0)
-    # v_yy = dde.grad.jacobian(v_y, x, i = 0, j = 1)
+  #   v_x = dde.grad.jacobian(v, x, i = 0, j = 0)
+  #   v_y = dde.grad.jacobian(v, x, i = 0, j = 1)
 
-    v_xx = dde.grad.hessian(v, x, component=0, i = 0, j = 0)
-    v_yy = dde.grad.hessian(v, x, component=0, i = 0, j = 1)
+  #   # u_xx = dde.grad.jacobian(u_x, x, i = 0, j = 0)
+  #   # u_yy = dde.grad.jacobian(u_y, x, i = 0, j = 1)
 
-    p_x = dde.grad.jacobian(p, x, i = 0, j = 0)
-    p_y = dde.grad.jacobian(p, x, i = 0, j = 1)
+  #   u_xx = dde.grad.hessian(u, x, component=0, i = 0, j = 0)
+  #   u_yy = dde.grad.hessian(u, x, component=0, i = 0, j = 1)
 
-    # x momentum
-    f1 = rho * (u * u_x + v * u_y) + p_x - mu * (u_xx + u_yy)
+  #   # v_xx = dde.grad.jacobian(v_x, x, i = 0, j = 0)
+  #   # v_yy = dde.grad.jacobian(v_y, x, i = 0, j = 1)
 
-    # y momentum
-    f2 = rho * (u * v_x + v * v_y) + p_y - mu * (v_xx + v_yy)
+  #   v_xx = dde.grad.hessian(v, x, component=0, i = 0, j = 0)
+  #   v_yy = dde.grad.hessian(v, x, component=0, i = 0, j = 1)
 
-    # continuity
-    # f3 = u_x + v_y # satisfies automatically
+  #   p_x = dde.grad.jacobian(p, x, i = 0, j = 0)
+  #   p_y = dde.grad.jacobian(p, x, i = 0, j = 1)
 
-    return f1, f2
+  #   # x momentum
+  #   f1 = rho * (u * u_x + v * u_y) + p_x - mu * (u_xx + u_yy)
+
+  #   # y momentum
+  #   f2 = rho * (u * v_x + v * v_y) + p_y - mu * (v_xx + v_yy)
+
+  #   # continuity
+  #   # f3 = u_x + v_y # satisfies automatically
+
+  #   return f1, f2
 
 
   def getU(self, x, y):
-    # return dde.grad.jacobian(y, x, i = 0, j = 1) 
-    return y[:, 0:1] 
+    return dde.grad.jacobian(y, x, i = 0, j = 1) 
+    # return y[:, 0:1] 
 
   def getV(self, x, y):
-    # return - dde.grad.jacobian(y, x, i = 0, j = 0)  
-    return y[:, 1:2] 
+    return - dde.grad.jacobian(y, x, i = 0, j = 0)  
+    # return y[:, 1:2] 
 
   def getP(sefl, x, y):
-    # return y[:, 1:2]
-    return y[:, 2:3]
+    return y[:, 1:2]
+    # return y[:, 2:3]
 
 
   # Boundaries definition
